d862b4d05ec64458505e2467dc5bae5f05d4135b
pamelatsabou <99961651+pamelatsabou@users.noreply.github.com>
2022-04-05 23:29:16 +0100

    Seconds to yrs, days, hrs, mins, secs

--- main.py
+++ main.py
@@ -1,70 +1,20 @@
-# Race game using dice
+# Easy mode
 
-import random
+number_in_seconds = int(input("Enter the number of seconds to be converted: "))
+seconds_in_a_year = 31536000
+seconds_in_a_day = 86400
+seconds_in_an_hour = 3600
+seconds_in_a_minute = 60
 
-dice = [1, 2, 3, 4, 5, 6]
-your_steps = 0
-computer_steps = 0
-is_over = False
-play_again = True
-points = 0
+years = number_in_seconds // seconds_in_a_year
+first_remainder = number_in_seconds % seconds_in_a_year
+days = first_remainder // seconds_in_a_day
+second_remainder = first_remainder % seconds_in_a_day
+hours = second_remainder // seconds_in_an_hour
+third_remainder = second_remainder % seconds_in_an_hour
+minutes = third_remainder // seconds_in_a_minute
+fourth_remainder = third_remainder % seconds_in_a_minute
+seconds = fourth_remainder
 
-print("Welcome to the race game!\n"
-      "You'll be rolling a dice in order to move along the steps.\n"
-      "You have between 30 and 35 steps to reach the finish line.")
-play = input("Do you want to play the race game alone or against the computer? Type 'a' or 'c': ")
+print(f"The time you entered is {years}:{days}:{hours}:{minutes}:{seconds}")
 
-
-# Playing alone
-
-if play == 'a':
-    while play_again:
-        difficulty = input("Enter difficulty level, 'hard' or 'easy': ")
-        if difficulty == 'hard':
-            moves = 8
-        elif difficulty == 'easy':
-            moves = 10
-
-        while not is_over:
-            roll_number = random.choice(dice)
-            your_steps += roll_number
-            moves -= 1
-            print(f"You are at step {your_steps}, left with {moves} moves.")
-            if moves == 0:
-                is_over = True
-                print("\nYou went out of moves.")
-                if 30 <= your_steps <= 35:
-                    points += 1
-                    print(f"You arrived still, you win with {points} point.")
-                else:
-                    print("You lose")
-            elif 30 <= your_steps <= 35:
-                is_over = True
-                points += 3
-                print(f"\nYou arrived on time, you win with {points} points.")
-        decision = input("Do you want to race again?")
-        if decision == 'n':
-            play_again = False
-            print(f"Total points: {points}")
-        else:
-            is_over = False
-            your_steps = 0
-
-
-# Playing against computer
-elif play == 'c':
-    while not is_over:
-        your_roll_number = random.choice(dice)
-        computer_roll_number = random.choice(dice)
-        your_steps += your_roll_number
-        computer_steps += computer_roll_number
-        print(f"You are at step {your_steps}, opponent at step {computer_steps}.")
-        if (30 <= computer_steps <= 35) and (your_steps < 30 or your_steps > 35):
-            is_over = True
-            print("\nOpponent arrived before you, you lose")
-        elif (30 <= your_steps <= 35) and (computer_steps < 30 or computer_steps > 35):
-            is_over = True
-            print("\nYou arrived before opponent, you win")
-        elif 30 <= computer_steps <= 35 and 30 <= your_steps <= 35:
-            is_over = True
-            print("\nYou arrived at the same time, it's a draw.")
