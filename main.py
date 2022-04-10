858772b4623d59511b11a6bed537fe2e3d48ef1d
pamelatsabou <99961651+pamelatsabou@users.noreply.github.com>
2022-04-10 21:10:03 +0100

    star_draw with turtle

--- main.py
+++ main.py
@@ -1,20 +1,14 @@
-# Easy mode
+# Star
 
-number_in_seconds = int(input("Enter the number of seconds to be converted: "))
-seconds_in_a_year = 31536000
-seconds_in_a_day = 86400
-seconds_in_an_hour = 3600
-seconds_in_a_minute = 60
+from turtle import Turtle, Screen
 
-years = number_in_seconds // seconds_in_a_year
-first_remainder = number_in_seconds % seconds_in_a_year
-days = first_remainder // seconds_in_a_day
-second_remainder = first_remainder % seconds_in_a_day
-hours = second_remainder // seconds_in_an_hour
-third_remainder = second_remainder % seconds_in_an_hour
-minutes = third_remainder // seconds_in_a_minute
-fourth_remainder = third_remainder % seconds_in_a_minute
-seconds = fourth_remainder
+pen = Turtle()
+for _ in range(5):
+    pen.left(72)
+    pen.forward(150)
+    pen.right(144)
+    pen.forward(150)
 
-print(f"The time you entered is {years}:{days}:{hours}:{minutes}:{seconds}")
 
+my_screen = Screen()
+my_screen.exitonclick()
